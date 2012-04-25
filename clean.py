e451b848e474a794a45c08109abf2415c85147a6
Jonathan Hadisuryo <jonathan.hadisuryo@gmail.com>
2012-04-25 01:10:13 -0700

    new data

---
+++ clean.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+from __future__ import division
+import math
+import sys
+import re
+
+import numpy as np
+from pandas import *
+
+def clean(df):
+    d1 = df.drop(['a'], axis=1)
+    d2 = d1.drop(['b'], axis=1)
+    d3 = d2.drop(['c'], axis=1)
+    d4 = d3.drop(['d'], axis=1)
+    d5 = d4.drop(['e'], axis=1)
+    d6 = d5.drop(['f'], axis=1)
+    d7 = d6.drop(['fips'], axis=1)
+    d8 = d7[d7["Year"] > 2003]
+    d9 = d8[d8["Year"] < 2009]
+    d9.to_csv("./test.csv",index=False)
+
+def main(*args):
+  df = read_csv('lifeExpectancy_perYear.csv')
+  clean(df)
+
+if __name__ =='__main__':
+    sys.exit(main(*sys.argv[1:]))
